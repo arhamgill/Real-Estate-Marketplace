b6f90cceae923c92f21e649f3e041b3fdc8b5a7a
arhamgill <arhamgillcoder@gmail.com>
2024-12-06 00:31:34 +0500

    errro middleware cretaed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +9,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    next(error);
   }
 };
